6daf60bc9a206a151ab82e5aab3f16f0efeb4dbb
andrei.spitsin <andrei.spitsin@fingular.com>
2025-01-27 22:36:23 +0700

    release graph, linked-list, queue, stack and setup tests

--- data-structures/graph/graph.test.ts
+++ data-structures/graph/graph.test.ts
@@ -1,18 +1,33 @@
 import {UndirectedGraph} from "./graph";
 
 describe("Graph", () => {
-    it("should create graph correctly", async () => {
+    it("should create graph correctly", () => {
         const graph = new UndirectedGraph();
 
         expect(graph).toBeTruthy();
     })
 
-    it("should return error if add edge to unexisted vertexes", async () => {
+    it("should return error if add edge to unexisted vertexes", () => {
         const graph = new UndirectedGraph();
 
         expect(() => graph.addEdge('A', 'B')).toThrowError('В графе нет таких вершин')
+    })
 
+    it("should return error if start dfs from unexisted vertex", () => {
+        const graph = new UndirectedGraph();
+        const mockCallback = jest.fn(x => 42 + x);
+        expect(() => graph.dfs('A', mockCallback)).toThrowError('В графе нет таких вершин')
+    })
 
+    it("should call callback same times as graph vertexes count", () => {
+        const graph = new UndirectedGraph();
+        const mockCallback = jest.fn(x => 42 + x);
+
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addEdge('A', 'B');
+        graph.dfs('A', mockCallback)
+        expect(mockCallback.mock.calls).toHaveLength(2);
     })
 
 })
--- data-structures/graph/graph.ts
+++ data-structures/graph/graph.ts
@@ -30,14 +30,59 @@ export class UndirectedGraph<T = string | number> {
         }
     }
 
-    dfs(startVertex: T, callback: (vertex: T) => void) {
+
+
+    dfs(startVertex: T, callback?: (vertex: T) => void) {
         // этот метод содержит итеративную реализацию поиска в глубину (depth-first search)
         let list = this.vertices; // список смежности
         let stack = [startVertex]; // стек вершин для перебора
-        let visited = { [startVertex]: 1 }; // посещенные вершины
+        let visited = new Map().set(startVertex, 1); // посещенные вершины
+
+        if(!list.has(startVertex)) {
+            throw new Error('В графе нет таких вершин');
+        }
+        const handleVertex = (vertex: T) => {
+            if(callback) {
+                callback(vertex);
+            }
+
+            // получаем список смежных вершин
+            const neighboursList = list.get(vertex) as T[];
+            const reversedNeighboursList = neighboursList.reverse();
+
+
+            reversedNeighboursList.forEach(neighbour => {
+                if (!visited.has(neighbour)) {
+                    // отмечаем вершину как посещенную
+                    visited.set(neighbour, 1);
+                    // добавляем в стек
+                    stack.push(neighbour);
+                }
+            });
+        }
+
+        while(stack.length) {
+            let activeVertex = stack.pop() as T;
+            handleVertex(activeVertex);
+        }
+
+        // проверка на изолированные фрагменты
+        stack = Array.from(list.keys());
+
+        while(stack.length) {
+            let activeVertex = stack.pop() as T;
+            if (!visited.has(activeVertex)) {
+                visited.set(activeVertex, 1);
+                handleVertex(activeVertex);
+            }
+        }
+
     }
+
 
     bfs() {
         // этот метод содержит итеративную реализацию поиска в ширину (breadth-first-search)
     }
 }
+
+
